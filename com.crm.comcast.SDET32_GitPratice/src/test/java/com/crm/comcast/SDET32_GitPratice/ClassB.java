4d629ce626cec5eacf3c0870d665330f978e9dda
HOME <HOME@LAPTOP-BT5LSUNM>
2022-04-12 12:23:42 +0530

    Commit 2

---
+++ com.crm.comcast.SDET32_GitPratice/src/test/java/com/crm/comcast/SDET32_GitPratice/ClassB.java
@@ -0,0 +1,14 @@
+package com.crm.comcast.SDET32_GitPratice;
+
+import org.testng.annotations.Test;
+
+public class ClassB {
+
+	@Test
+	public void demoTest() {
+		
+		System.out.println("Push 2");
+		System.out.println("Push 3");
+
+	}
+}
