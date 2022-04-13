ef5a3050412c2e9f7ac029e922c8f9dab784308c
VishalBTM <96617070+VishalBTM@users.noreply.github.com>
2022-04-13 20:02:55 +0530

    Update ClassC.java

--- com.crm.comcast.SDET32_GitPratice/src/test/java/com/crm/comcast/SDET32_GitPratice/ClassC.java
+++ com.crm.comcast.SDET32_GitPratice/src/test/java/com/crm/comcast/SDET32_GitPratice/ClassC.java
@@ -11,8 +11,7 @@ public class ClassC {
 
 		System.out.println("sixth push")
 		System.out.println("seventh push");
-		System.out.println("11th push");
-	        System.out.println("12th push");
+		
 
 
 	}
